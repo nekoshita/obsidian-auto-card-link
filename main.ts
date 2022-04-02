26826feb7d31380dc040fba6a792fc1a2d60042b
Nekoshita Yuki <>
2022-04-02 16:50:26 +0900

    chore: install prettier

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import { Plugin, Notice } from 'obsidian';
 
 interface PasteFunction {
-    (this: HTMLElement, ev: ClipboardEvent): void;
-  }
-  
+  (this: HTMLElement, ev: ClipboardEvent): void;
+}
+
 export default class ObsidianOGP extends Plugin {
-    async onload() {
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian OGP', () => {
-			new Notice('Hello world!');
-		});
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
-    }
+  async onload() {
+    const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian OGP', () => {
+      new Notice('Hello world!');
+    });
+    ribbonIconEl.addClass('my-plugin-ribbon-class');
+  }
 }
